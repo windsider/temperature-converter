7e0d0b52875f9fed1be5e860f322de45c756b530
Andrey Grechukhin <vrzherdev@gmail.com>
2017-12-22 20:07:17 +0300

    modified pom, added cucumber glue code and step defifnitions file

---
+++ src/test/java/com/packtpub/mastering/selenium/steps/PendingException.java
@@ -0,0 +1,11 @@
+package com.packtpub.mastering.selenium.steps;
+
+/**
+ * Created by ruvzherdev on 12/22/2017.
+ */
+public class PendingException extends Throwable {
+
+    public PendingException(String message) {
+        super(message);
+    }
+}
