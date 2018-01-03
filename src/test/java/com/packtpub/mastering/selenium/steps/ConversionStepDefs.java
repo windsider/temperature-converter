65e514d7f15474051f3ea4eb8618ecc5f73f4983
Andrey Grechukhin <vrzherdev@gmail.com>
2018-01-03 19:38:05 +0300

    new files

--- src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
+++ src/test/java/com/packtpub/mastering/selenium/steps/ConversionStepDefs.java
@@ -1,5 +1,7 @@
 package com.packtpub.mastering.selenium.steps;
 
+import pageobjects.Google;
+import pageobjects.TemperatureConverterPage;
 import cucumber.annotation.After;
 import cucumber.annotation.Before;
 import cucumber.annotation.en.Given;
@@ -13,26 +15,31 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 /**
- * Created by ruvzherdev on 12/22/2017.
+ * Created by Ripon on 11/26/2015.
  */
 public class ConversionStepDefs {
-    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$ _cool")
+    private WebDriver driver;
+    public Google googlePage;
+    public TemperatureConverterPage temperatureConverterPage;
+    @Given("^I want to  convert (\\d+).(\\d+) degree Fahrenheit to Celsius$")
     public void I_want_to_convert_degree_Fahrenheit_to_Celsius(int arg1, int arg2) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        fail("It is failed");
-        throw new PendingException("PendingError");
+        driver = new FirefoxDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        googlePage = new Google(driver);
+        temperatureConverterPage = googlePage.goToTemperatureConversionPage();
     }
 
     @When("^I input the value of Fahrenheit as (\\d+).(\\d+) in text field$")
     public void I_input_the_value_of_Fahrenheit_as_in_text_field(int arg1, int arg2) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-
-        throw new PendingException("PendingError");
+        //input Fahrenheit value
+        temperatureConverterPage.inputFahrenheit(98.6);
     }
 
     @Then("^It should be converted to Celsius as (\\d+) degree$")
     public void It_should_be_converted_to_Celsius_as_degree(int arg1) throws Throwable {
-        // Express the Regexp above with the code you wish you had
-        throw new PendingException("PendingError");
+        //assertion
+        assertEquals(temperatureConverterPage.actualResult(), temperatureConverterPage.expectedResult());
+        driver.quit();
     }
 }
